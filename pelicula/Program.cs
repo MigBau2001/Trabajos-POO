f4295ede98d6e072d17599a09c22bef96a513fd4
Miguel Bautista <3bmbdma@gmail.com>
2020-02-10 20:01:44 -0800

    Actividad 2 (Primera versión)
    Se crean dos objetos de la clase Pelicula, se asignan atributos y posteriormente se imprime en consola el titulo y el año.

---
+++ pelicula/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pelicula
+{
+
+    class Pelicula
+    {
+        public string titulo;
+        public Int16 año;
+        public string pais;
+        public string director;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula peli1 = new Pelicula();
+            peli1.titulo = "Toy story 4";
+            peli1.año = 2019;
+            peli1.pais = "Estados unidos";
+            peli1.director = "Josh Cooley";
+
+            Pelicula peli2 = new Pelicula();
+            peli2.titulo = "Parasite";
+            peli2.año = 2019;
+            peli2.pais = "Corea del sur";
+            peli2.director = "Bong Joon-ho";
+
+            Console.WriteLine("Titulo de la primera pelicula: " + peli1.titulo);
+            Console.WriteLine("Año de la primera pelicula: " + peli1.año);
+            Console.WriteLine("");
+            Console.WriteLine("Titulo de la segunda pelicula: " + peli2.titulo);
+            Console.WriteLine("Año de la segunda pelicula: " + peli2.año);
+        }
+    }
+}
