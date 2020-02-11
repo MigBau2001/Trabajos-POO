8c689b16fc8d07b3c7c761d38742e0ed47069cf6
Miguel Bautista <3bmbdma@gmail.com>
2020-02-10 20:24:54 -0800

    Update Program.cs

--- pelicula/Program.cs
+++ pelicula/Program.cs
@@ -46,11 +46,10 @@ namespace pelicula
             peli2.pais = "Corea del sur";
             peli2.director = "Bong Joon-ho";
 
-            Console.WriteLine("Titulo de la primera pelicula: " + peli1.getTitulo());
-            Console.WriteLine("Año de la primera pelicula: " + peli1.getAño());
+            Console.WriteLine("{0}({1})", peli1.getTitulo(), peli1.getAño());
             Console.WriteLine("");
-            Console.WriteLine("Titulo de la segunda pelicula: " + peli2.getTitulo());
-            Console.WriteLine("Año de la segunda pelicula: " + peli2.getAño());
+            Console.WriteLine("{0}({1})", peli2.getTitulo(), peli2.getAño());
+
         }
     }
 }
