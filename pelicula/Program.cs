f966cf5516f30bdb2fa7e8007aee979fff00b737
Miguel Bautista <3bmbdma@gmail.com>
2020-02-22 08:50:46 -0800

    Actualización con impresión de nombre
    Se hicieron modificaciones necesarias para implementar la lista peliculas

--- pelicula/Program.cs
+++ pelicula/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace pelicula
 {
@@ -10,6 +11,11 @@ namespace pelicula
         public string pais;
         public string director;
 
+        public Pelicula(string titulo, Int16 año)
+        { 
+            this.titulo = titulo;
+            this.año = año;
+        }
         public void setTitulo(string titulo1)
         {
             titulo = titulo1;
@@ -30,27 +36,17 @@ namespace pelicula
             return año;
         }
 
-        public Pelicula()
-        {
-
-        }
-
-        public Pelicula(string titulo, Int16 año)
-        { 
-            this.titulo = titulo;
-            this.año = año;
-        }
-
         public void Imprime()
         {
             Console.WriteLine("{0}({1})", titulo, año);
+            
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Pelicula peli1 = new Pelicula();
+            /* Pelicula peli1 = new Pelicula();
             peli1.setTitulo("Toy story 4");
             peli1.setAño(2019);
             peli1.pais = "Estados unidos";
@@ -61,21 +57,21 @@ namespace pelicula
             peli2.setAño(2019);
             peli2.pais = "Corea del sur";
             peli2.director = "Bong Joon-ho";
+            */
 
-            Console.WriteLine("{0}({1})", peli1.getTitulo(), peli1.getAño());
+            /*Console.WriteLine("{0}({1})", peli1.getTitulo(), peli1.getAño());
             Console.WriteLine("");
             Console.WriteLine("{0}({1})", peli2.getTitulo(), peli2.getAño());
-
+            */
             List <Pelicula> peliculas = new List<Pelicula>();
-            peliculas.add("Roma", 2018);
-            peliculas.add("Kung Fu Panda", 2008);
-            peliculas.add("Black Panther", 2018);
-            peliculas.add("Spider man 2", 2004);
-            peliculas.add("50 sombras de Grey", 2015);
+            peliculas.Add(new Pelicula("Roma", 2018));
+            peliculas.Add(new Pelicula("Kung Fu Panda", 2008));
+            peliculas.Add(new Pelicula("Black Panther", 2018));
+            peliculas.Add(new Pelicula("Spider man 2", 2004));
+            peliculas.Add(new Pelicula("50 sombras de Grey", 2015));
 
-            foreach(Peliculas a in peliculas);
-
-            
+            foreach(Pelicula a in peliculas)
+            a.Imprime();
 
 
         }
